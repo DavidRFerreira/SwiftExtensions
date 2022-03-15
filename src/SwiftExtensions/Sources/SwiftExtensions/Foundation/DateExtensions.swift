ef045027355a616ac9374aa86e7d964227e7450e
David Ferreira <davidferreira@MacBook-Pro-de-David-3.local>
2022-03-15 18:22:09 +0000

    Adds method to convert Date to String with a given format

---
+++ src/SwiftExtensions/Sources/SwiftExtensions/Foundation/DateExtensions.swift
@@ -0,0 +1,26 @@
+//
+//  DateExtensions.swift
+//  
+//
+//  Created by David Ferreira on 15/03/2022.
+//
+
+#if canImport(Foundation) 
+
+import Foundation
+
+
+extension Date {
+    
+    // Converts to string with specified format. 
+    func convertToStringWithFormat(dateFormat: String) -> String {
+        let formatter = DateFormatter()
+        formatter.locale = NSLocale.current
+        formatter.dateFormat = dateFormat
+        return formatter.string(from: self)
+    }
+    
+}
+
+
+#endif
